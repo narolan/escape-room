7ab41e5e03996977c9b9421e607699bc49eb3ee5
jleus <jorgi.leus@persgroep.net>
2025-07-21 19:32:32 +0200

    redirect attempt #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import './App.css'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 import AdminPage from "./pages/AdminPage.jsx";
 import OverviewPage from "./pages/OverviewPage.jsx";
 import GroupPage from "./pages/GroupPage.jsx";
+import {useEffect} from "react";
 
 function App() {
+
+    let data = null;
+
+    useEffect(() => {
+        const saved = localStorage.getItem("escapeRoomData");
+        if (!saved) {
+            return;
+        }
+
+        data = JSON.parse(saved);
+    }, []);
+
+    localStorage.getItem("escapeRoomData")
+
     return (
         <>
             <div className="background-overlay"></div>
             <div className="page-content">
                 <Router basename="/escape-room">
                     <Routes>
-                        <Route path="/" element={<AdminPage/>}/>
+                        {
+                            data ?
+                                <Route path="/" element={<Navigate to="/overview" replace/>} />
+                                :
+                                <Route path="/" element={<AdminPage/>}/>
+                        }
                         <Route path="/overview" element={<OverviewPage/>}/>
                         <Route path="/group/:id" element={<GroupPage/>}/>
                     </Routes>
